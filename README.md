d03da8ce78551d4f5830aaa3726d7ac4e84ea620
Andreas Scherbaum <andreas@scherbaum.biz>
2019-02-23 18:10:37 +0100

    Add cert age filter plugin

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # ansible_check_cert_age
+
 Ansible filter which checks the age (remaining valid time) of a certificate
+
+
+# Installation
+
+Create a subfolder _filter_plugins_ in your Playbook directory. Place the file _cert_age.py_ in _filter_plugins_.
+
+
+# Usage
+
+In your Playbook, specify this filter on the certificate filename, and supply the minimum number of days the cert must be valid.
+
+## Example 1:
+
+```
+- name: Cert is valid
+  debug:
+    msg: "{{ ('/path/to/signed.crt')|check_cert_age(15) }}
+
+```
+
+This will print _0_ if the cert is valid for the number days, and _1_ otherwise.
+
+
+## Example 2:
+
+```
+- name: Renew cert
+  shell: ...
+  when: ('/path/to/signed.crt')|check_cert_age(15) == "1"
+```
+
+This will execute the Play when the cert is about to expire.
---
+++ cert_age.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from ansible.errors import AnsibleError
+import os
+import sys
+from ansible.module_utils._text import to_native
+from ansible.module_utils._text import to_text
+import subprocess
+
+
+class FilterModule(object):
+    ''' Query filter '''
+
+    def filters(self):
+        return {
+            'check_cert_age': self.check_cert_age
+        }
+
+
+    def check_cert_age(self, cert_path, remaining_days):
+        '''Query a certificate (1st parameter) and check if the remaining valid
+           time is at least as many days as specified in the 2nd parameter
+           Returns 0 if the cert is valid for the specified number of days
+           Returns 0 if the cert is not valid for that time
+        '''
+
+        # first check if the cert exists
+        if (os.path.exists(cert_path) is False):
+            raise AnsibleError('Not found: %s' % to_native(cert_path))
+        if (os.access(cert_path, os.R_OK) is False):
+            raise AnsibleError('No access: %s' % to_native(cert_path))
+
+        # there is a slight chance that the file goes away between the time
+        # existence and access is checked, but that's ok
+        # raising the proper error in the majority of cases is more important
+
+        remaining_time = int(remaining_days) * 86400
+        d0 = open(os.devnull, 'w')
+        result = subprocess.call(['/usr/bin/openssl', 'x509', '-in', cert_path, '-noout', '-checkend', str(remaining_time)], stdout = d0, stderr = d0)
+        if (result != 0):
+            # make sure we return '1' for an expiring certificate, or for every error
+            result = 1
+
+        return(to_text(str(result)))
